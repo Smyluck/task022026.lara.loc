9a6993c0331f16a55993a3d6cac8ba7febb7f5ce
codefree <veb-master.veb@yandex.ru>
2026-01-25 12:20:13 +0000

    рабочий vite

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    server: {
-        port: 5179,
-    },
     plugins: [
         laravel({
             input: "resources/js/app.js",
@@ -20,6 +16,5 @@ export default defineConfig({
                 },
             },
         }),
-        tailwindcss(),
     ],
 });
